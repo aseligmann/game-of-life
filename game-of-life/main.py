6be5c13f1b23007458270bd242d57243e2bc11b2
Albert Seligmann <albertsseligmann@gmail.com>
2023-07-31 19:46:42 +0200

    Larger starting grid

--- game-of-life/main.py
+++ game-of-life/main.py
@@ -10,18 +10,21 @@ from renderer_cv2 import Renderer
 
 
 if __name__ == "__main__":
-    size = 19
+    size = 200
 
     grid = Grid()
     grid.empty(size)
-    Pulsar().populate(grid, int(size / 2), int(size / 2))
+    for i in range(10, size, 20):
+        for j in range(10, size, 20):
+            grid.populate(i, j)
+            Pulsar().populate(grid, i, j)
 
     renderer = Renderer(size)
     game = Game(grid=grid)
 
     def update():
-        game.tick()
         renderer.render(game.getState3D())
+        game.tick()
 
     for i in range(100):
         update()
